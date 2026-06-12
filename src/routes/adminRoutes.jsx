1bbee36437efd11cbc898181678a82ce552f04db
V24-z <vidhizalavadiya11@gmail.com>
2026-06-12 16:51:12 +0530

    Fix:lazy loading

--- src/routes/adminRoutes.jsx
+++ src/routes/adminRoutes.jsx
@@ -1,24 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-//import AdminLayout from "../admin/layouts/AdminLayout";
-//import Dashboard from "../admin/pages/Dashboard";
-//import Products from "../admin/pages/Products";
-//import Banner from "../admin/pages/Banner.jsx";
-//import Users from "../admin/pages/Users.jsx";
-//import AdminRoute from "./adminroute.jsx";
-//import Category from "../admin/pages/category.jsx";
-import { lazy, Suspense } from "react";
-const AdminLayout=lazy(()=>import ('../admin/layouts/AdminLayout.jsx'));
-const Dashboard=lazy(()=>import ('../admin/pages/Dashboard.jsx'));
-const Products=lazy(()=>import('../admin/pages/Products.jsx'));
-const Banner=lazy(()=>import ('../admin/pages/Banner.jsx'));
-const Users=lazy(()=>import ('../admin/pages/Users.jsx'));
-const AdminRoute=lazy(()=>import ('./adminroute.jsx'));
-const Category=lazy(()=>import ('../admin/pages/category.jsx'));
+import AdminLayout from "../admin/layouts/AdminLayout";
+import Dashboard from "../admin/pages/Dashboard";
+import Products from "../admin/pages/Products";
+import Banner from "../admin/pages/Banner.jsx";
+import Users from "../admin/pages/Users.jsx";
+import AdminRoute from "./adminroute.jsx";
+import Category from "../admin/pages/category.jsx";
+
+
 
 const AdminRoutes = () => {
-  //console.log("adminroutes")
+  console.log("adminroutes")
   return (
-    <Suspense fallback={<div>Loading</div>}>
     <Routes>
       <Route
         path="/"               // <-- Relative to /admin/* in App.jsx
@@ -36,7 +29,6 @@ const AdminRoutes = () => {
   {/* /admin/users */}
       </Route>
     </Routes>
-    </Suspense>
   );
 };
 
--- src/routes/userRoutes.jsx
+++ src/routes/userRoutes.jsx
@@ -1,22 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-//import Layout from "../components/layout.jsx";
-//import Home from "../Pages/home.jsx";
-//import About from "../Pages/about.jsx";
-//import Contact from "../Pages/contact.jsx";
-//import ViewSingleProduct from "../Pages/viewdetail.jsx";
-//import Cart from "../Pages/cart.jsx";
-//import Profile from "../components/user/Profile.jsx"
-import { lazy, Suspense } from "react";
-const Layout = lazy(() => import('../components/layout.jsx'));
-const Home = lazy(() => import('../Pages/home.jsx'));
-const About = lazy(() => import('../Pages/about.jsx'));
-const Contact = lazy(() => import('../Pages/contact.jsx'));
-const ViewSingleProduct = lazy(() => import('../Pages/viewdetail.jsx'));
-const Cart = lazy(() => import('../Pages/cart.jsx'));
-const Profile = lazy(() => import('../components/user/Profile.jsx'));
+import Layout from "../components/layout.jsx";
+import Home from "../Pages/home.jsx";
+import About from "../Pages/about.jsx";
+import Contact from "../Pages/contact.jsx";
+import ViewSingleProduct from "../Pages/viewdetail.jsx";
+import Cart from "../Pages/cart.jsx";
+import Profile from "../components/user/Profile.jsx"
 function UserRoutes() {
   return (
-    <Suspense fallback={<div>Loading....</div>}>  
     <Routes>
       {/* Pages with header/footer */}
       <Route element={<Layout />}>
@@ -28,7 +19,6 @@ function UserRoutes() {
       <Route path="/viewdetail/:id" element={<ViewSingleProduct />} />
       <Route path="/cart" element={<Cart />} />
     </Routes>
-    </Suspense>
   );
 }
 
