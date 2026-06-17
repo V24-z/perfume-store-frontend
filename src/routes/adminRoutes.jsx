d0e81b28805e7310e2c9257e8664258823a751d5
V24-z <vidhizalavadiya11@gmail.com>
2026-06-17 14:56:06 +0530

    add lazy loading at adminroute

--- src/routes/adminRoutes.jsx
+++ src/routes/adminRoutes.jsx
@@ -1,17 +1,27 @@
 import { Routes, Route } from "react-router-dom";
-import AdminLayout from "../admin/layouts/AdminLayout";
-import Dashboard from "../admin/pages/Dashboard";
-import Products from "../admin/pages/Products";
-import Banner from "../admin/pages/Banner.jsx";
-import Users from "../admin/pages/Users.jsx";
-import AdminRoute from "./adminroute.jsx";
-import Category from "../admin/pages/category.jsx";
+//import AdminLayout from "../admin/layouts/AdminLayout";
+//import Dashboard from "../admin/pages/Dashboard";
+//import Products from "../admin/pages/Products";
+//import Banner from "../admin/pages/Banner.jsx";
+//import Users from "../admin/pages/Users.jsx";
+//import AdminRoute from "./adminroute.jsx";
+//import Category from "../admin/pages/category.jsx";
+import { lazy, Suspense } from "react";
 
+const AdminLayout =lazy(()=>import('../admin/layouts/AdminLayout.jsx'));
+
+const Dashboard=lazy(()=>import('../admin/pages/Dashboard.jsx'));
+const Products =lazy(()=>import('../admin/pages/Products.jsx'));
+const Banner =lazy(()=>import('../admin/pages/Banner.jsx'));
+const Users =lazy(()=>import('../admin/pages/Users.jsx'));
+const Category =lazy(()=>import('../admin/pages/category.jsx'));
+const  AdminRoute=lazy(()=>import('./adminroute.jsx'));
 
 
 const AdminRoutes = () => {
-  console.log("adminroutes")
+  
   return (
+    <Suspense rollback={<div>Loadin...</div>}>
     <Routes>
       <Route
         path="/"               // <-- Relative to /admin/* in App.jsx
@@ -29,6 +39,7 @@ const AdminRoutes = () => {
   {/* /admin/users */}
       </Route>
     </Routes>
+    </Suspense>
   );
 };
 
