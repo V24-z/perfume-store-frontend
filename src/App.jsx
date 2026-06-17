0862b8e8c2d6b4d7c81687af4ea08fa97b44df22
V24-z <vidhizalavadiya11@gmail.com>
2026-06-17 15:04:09 +0530

    add lazy loading at app

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import AdminRoutes from "./routes/adminRoutes";
-import UserRoutes from "./routes/userRoutes.jsx";
-import Login from "./components/user/login.jsx";
-import Signup from "./components/user/signup.jsx";
+//import AdminRoutes from "./routes/adminRoutes";
+//import UserRoutes from "./routes/userRoutes.jsx";
+//import Login from "./components/user/login.jsx";
+//import Signup from "./components/user/signup.jsx";
 import { useAuth } from "./context/useAuth.jsx";
 import { Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+const AdminRoutes = lazy(() => import("./routes/adminRoutes.jsx"));
+const UserRoutes = lazy(() => import("./routes/userRoutes.jsx"));
+const Login = lazy(() => import("./components/user/login.jsx"));
+const Signup = lazy(() => import("./components/user/signup.jsx"));
 
 function App() {
   const { user } = useAuth();
-console.log("APP USER:", user);
+
   return (
-    <Routes>
-       <Route path="/*" element={<UserRoutes />} />
-      
-      {!user && (
-        <>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </>
-      )}
+    <Suspense rollback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/*" element={<UserRoutes />} />
 
-      {user && user.role === "admin" && (
-        <Route path="/admin/*" element={<AdminRoutes />} />
-      )}
+        {!user && (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </>
+        )}
 
-      
+        {user && user.role === "admin" && (
+          <Route path="/admin/*" element={<AdminRoutes />} />
+        )}
 
-      {/* Optional: redirect all unknown paths */}
-      <Route path="*" element={user ? <UserRoutes /> : <Login />} />
-    </Routes>
+        {/* Optional: redirect all unknown paths */}
+        <Route path="*" element={user ? <UserRoutes /> : <Login />} />
+      </Routes>
+    </Suspense>
   );
 }
 
