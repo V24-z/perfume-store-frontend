5a8cd9e2b71f533851a5bd778b35c5d5ceacbbc3
vidhi <140423245+V24-z@users.noreply.github.com>
2026-06-27 03:41:24 -0700

    fix api of benner

--- src/admin/pages/Banner.jsx
+++ src/admin/pages/Banner.jsx
@@ -237,39 +237,42 @@ function Banner() {
           ))
         )}
       </div>
+     
+          {deleteId && (
+          <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+            <div className="bg-white rounded-xl p-6 w-96 shadow-xl">
+              <h2 className="text-xl font-semibold mb-2">
+                Delete Banner
+              </h2>
+
+              <p className="text-gray-600 mb-6">
+                Are you sure you want to delete this banner?
+                This action cannot be undone.
+              </p>
+
+              <div className="flex justify-end gap-3">
+                <button
+                  onClick={() => setDeleteId(null)}
+                  className="px-4 py-2 rounded bg-gray-200 hover:bg-gray-300"
+                >
+                  Cancel
+                </button>
+
+                <button
+                  onClick={() => handleDelete(deleteId)}
+                  className="px-4 py-2 rounded bg-red-600 text-white hover:bg-red-700"
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+       
     </div>
+
+    
   );
-  // Delete Confirmation Modal
-  {deleteId && (
-  <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
-    <div className="bg-white rounded-xl p-6 w-96 shadow-xl">
-      <h2 className="text-xl font-semibold mb-2">
-        Delete Banner
-      </h2>
-
-      <p className="text-gray-600 mb-6">
-        Are you sure you want to delete this banner?
-        This action cannot be undone.
-      </p>
-
-      <div className="flex justify-end gap-3">
-        <button
-          onClick={() => setDeleteId(null)}
-          className="px-4 py-2 rounded bg-gray-200 hover:bg-gray-300"
-        >
-          Cancel
-        </button>
-
-        <button
-          onClick={() => handleDelete(deleteId)}
-          className="px-4 py-2 rounded bg-red-600 text-white hover:bg-red-700"
-        >
-          Delete
-        </button>
-      </div>
-    </div>
-  </div>
-)}
 }
 
 export default Banner;
