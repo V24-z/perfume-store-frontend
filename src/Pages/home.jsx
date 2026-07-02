5aac6c83651fc2849fd18c2bf6d885ca566019d0
V24-z <vidhizalavadiya11@gmail.com>
2026-07-02 15:45:03 +0530

    fix serchbar and wishlist route

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -545,11 +545,11 @@ function Home() {
                           ) : (
                             <ShoppingCart size={20} />
                           )}
-                        </button>
+                        
 
                         {/* Display quantity if in cart */}
                         {cartItems.length > 0 && <span className="absolute -top-1 -right-1 text-white font-extrabold w-4 h-4 rounded-full flex items-center justify-center shadow-sm" style={{ background: "#f43f5e", fontSize: 8 }}>{cartItems.length}</span>}
-                        
+                        </button>
 
                         <Link
                           to={`/viewdetail/${product.id}`}
